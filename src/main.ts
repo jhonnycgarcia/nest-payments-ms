3f8da3465f9b59605cb32a96fe5e6b6c5a24263d
Jhonny <jhonnycgarcia@gmail.com>
2025-06-12 17:42:28 -0500

    Payment micro service - inicio

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
+
+async function bootstrap() {
+  const logger = new Logger('Payments-ms');
+  const app = await NestFactory.create(AppModule);
+  await app.listen(envs.PORT);
+  logger.log(`Payments-ms is running on port ${envs.PORT}`);
+}
+bootstrap();
---
+++ src/payments/payments.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { CreatePaymentDto } from './dto/create-payment.dto';
+import { UpdatePaymentDto } from './dto/update-payment.dto';
+
+@Controller('payments')
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post('create-payment-session')
+  createPaymentSession(@Body() createPaymentDto: CreatePaymentDto) {
+    return this.paymentsService.create(createPaymentDto);
+  }
+
+  @Get('success')
+  success() {
+    return {
+      ok: true,
+      message: 'Payment successful',
+    };
+  }
+
+  @Get('cancel')
+  cancel() {
+    return {
+      ok: true,
+      message: 'Payment cancelled',
+    };
+  }
+
+  @Post('webhook')
+  async stripeWebhook(@Body() body: any) {
+    return {
+      ok: true,
+      message: 'Webhook received',
+      body,
+    };
+  }
+
+}
