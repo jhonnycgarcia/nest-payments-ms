f57bd94b378fa09fa0f3cf4c5d2f0a9d3846031b
Jhonny <jhonnycgarcia@gmail.com>
2025-06-13 13:18:04 -0500

    Microservicio Híbrido - Rest Nats

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,7 +7,7 @@ interface EnvConfig {
   STRIPE_ENDPOINT_SECRET: string;
   STRIPE_SUCCESS_URL: string;
   STRIPE_CANCEL_URL: string;
-
+  NATS_SERVERS: string;
 }
 
 const envSchema = joi.object({
@@ -16,9 +16,13 @@ const envSchema = joi.object({
   STRIPE_ENDPOINT_SECRET: joi.string().required(),
   STRIPE_SUCCESS_URL: joi.string().required(),
   STRIPE_CANCEL_URL: joi.string().required(),
+  NATS_SERVERS: joi.array().items(joi.string()).required(),
 }).unknown(true);
 
-const { value: envConfig, error } = envSchema.validate(process.env);
+const { value: envConfig, error } = envSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(';'),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -32,4 +36,5 @@ export const envs = {
   STRIPE_ENDPOINT_SECRET: envVars.STRIPE_ENDPOINT_SECRET,
   STRIPE_SUCCESS_URL: envVars.STRIPE_SUCCESS_URL,
   STRIPE_CANCEL_URL: envVars.STRIPE_CANCEL_URL,
+  NATS_SERVERS: envVars.NATS_SERVERS,
 };
